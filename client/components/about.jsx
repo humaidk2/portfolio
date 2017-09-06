b106b4ffb2732be681bc4b326953d0c030b2f3ef
Humaid Khan <humaidk2@gmail.com>
2017-09-06 15:34:29 -0700

    move css to assets folder and add walking cat in city using css

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -8,6 +8,9 @@ export default class About extends React.Component {
   render () {
     return (
             <div className="about">
+              <div id="cielo" ></div>
+              <div id="edificios"></div>
+              <div id="luna"></div>
               <div className="about-photo"><img src="assets/profile2.jpg" /></div>
               <div className="about-bio">
                 <div><span>Hi!</span><br />My name is Humaid khan,  I am an enthusiastic Full Stack Developer that loves developing with the latest trends 
@@ -17,8 +20,11 @@ export default class About extends React.Component {
                 <br />Linkedin:<a target="_blank" href="linkedin.com/in/humaidk2">linkedin.com/in/humaidk2</a>
                 <br />Github:<a target="_blank" href="github.com/humaidk2">github.com/humaidk2</a>
                 </div>
+                <div id="marco">
+                  <div id="gato"></div>
+                </div>
               </div>
-              <div className="about-logo"><img src="assets/logo9.png" /></div>
+
             </div>);
   }
 }
